6d336578e0141059f52c9e7d22ae90ec13aa1a7a
Luís Assunção <assuncaolfi@gmail.com>
2024-01-23 01:49:31 -0300

    update site

--- src/blog/theme.py
+++ src/blog/theme.py
@@ -1,13 +1,25 @@
+from cycler import cycler
 from matplotlib import font_manager
-from matplotlib import style
 import seaborn.objects as so
 
 
 def set():
-    so.Plot.config.theme.update(style.library["Solarize_Light2"])
-    font_path = "../../assets/FiraCode-Regular.ttf"  # TODO include in library
+    font_path = "../../assets/FiraCode-Regular.ttf"
     font_manager.fontManager.addfont(font_path)
     prop = font_manager.FontProperties(fname=font_path)
-    so.Plot.config.theme.update(
-        {"font.family": "sans-serif", "font.sans-serif": prop.get_name()}
-    )
+    theme = {
+        "axes.facecolor": "#f8f5f0",
+        "axes.prop_cycle": cycler(
+            color=[
+                "#325d88",
+                "#8e8c84",
+                "#93c54b",
+                "#29abe0",
+                "#f47c3c",
+                "#d9534f",
+            ]
+        ),
+        "font.family": "sans-serif",
+        "font.sans-serif": prop.get_name(),
+    }
+    so.Plot.config.theme.update(theme)
